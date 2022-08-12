f0fd06407b70c1f2b9b4a40b2cc44388ed1baebf
Sorokovsky <Sorokovskys@ukr.net>
2022-08-12 19:17:09 +0300

    users

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -3,7 +3,6 @@ import { ObjectId } from "mongoose";
 import IUser from "src/utils/interfaces/User.interface";
 import CreateUserDTO from "./dto/create-user.dto";
 import UsersService from "./users.service";
-
 @Controller('/users')
 export default class UsersController{
     constructor(private userService:UsersService){}
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -3,7 +3,6 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "src/schemas/user.schema";
 import UsersController from "./users.controller";
 import UsersService from "./users.service";
-
 @Module({
     controllers: [UsersController],
     imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
